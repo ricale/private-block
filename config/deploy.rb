4721891a8db38cc2eacc28306e4ef3d129b46da3
KIM Kangseong <KIM.Kangseong@gmail.com>
2016-09-10 16:07:51 +0900

    Update package.json for deploying

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,6 +29,8 @@ set :rvm_type, :system
 # Default value for :linked_files is []
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
+set :npm_flags, '--production --no-progress'    # default
+
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
 
