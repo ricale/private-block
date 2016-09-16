475779e96c53cfacd3338d677acdcbd23fcfc79b
KIM Kangseong <KIM.Kangseong@gmail.com>
2016-09-16 19:31:57 +0900

    Fix MyHelmet for og

--- app/assets/javascripts/components/commons/MyHelmet.js
+++ app/assets/javascripts/components/commons/MyHelmet.js
@@ -3,18 +3,46 @@ import Helmet from 'react-helmet'
 
 export default class MyHelmet extends Component {
   render () {
-    const { writing, title } = this.props
+    const { writing } = this.props
 
-    const helmetTitle = writing ? writing.title : title
+    let title = this.props.title
+    let type = 'website'
+    let description = 'Written by ricale'
+
+    // if(writing) {
+    //   title = writing.title
+    //   type = 'article'
+
+    //   const DESCRIPTION_MAX_LENGTH = 66
+    //   const firstLineBreakIndex  = writing.content.indexOf("\n")
+    //   const firstWhiteSpaceIndex = writing.content.indexOf(" ", DESCRIPTION_MAX_LENGTH)
+
+    //   let descriptionLength = DESCRIPTION_MAX_LENGTH
+
+    //   if(firstLineBreakIndex !== -1) {
+    //     if(firstLineBreakIndex < DESCRIPTION_MAX_LENGTH) {
+    //       descriptionLength = firstLineBreakIndex
+
+    //     } else if(firstWhiteSpaceIndex !== -1) {
+    //       descriptionLength = firstWhiteSpaceIndex < firstLineBreakIndex ? firstWhiteSpaceIndex : firstLineBreakIndex
+    //     }
+    //   }
+
+    //   description = writing.content.slice(0, descriptionLength)
+    // }
+
     const url = window.location.href
 
     return (
-      <Helmet title={helmetTitle}
+      <Helmet htmlAttributes={{"prefix": "og: http://ogp.me/ns# article: http://ogp.me/ns/article#"}}
+              title={title}
               titleTemplate='%s :: weblog ricale st.'
               defaultTitle='weblog ricale st.'
               meta={[
-                {"property": "og:title", "content": helmetTitle},
-                {"property": "og:url",   "content": url},
+                {"property": "og:title",       "content": title},
+                {"property": "og:url",         "content": url},
+                {"property": "og:type",        "content": type},
+                {"property": "og:description", "content": description},
               ]} />
     )
   }
