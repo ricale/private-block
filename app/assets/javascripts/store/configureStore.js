bfdc9a541baf5ab69046a810f49e80a41949d68d
KIM Kangseong <KIM.Kangseong@gmail.com>
2016-09-29 01:26:21 +0900

    Update env config for node_env

--- app/assets/javascripts/store/configureStore.js
+++ app/assets/javascripts/store/configureStore.js
@@ -1,3 +1,6 @@
+console.log('process', process)
+console.log('process.env', process.env)
+console.log('process.env.NODE_ENV', process.env.NODE_ENV)
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./configureStore.prod')
 } else {
