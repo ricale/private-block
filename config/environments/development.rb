eec7b54bd70e8c1c0f2a56162ac22daa027ac341
KIM Kangseong <KIM.Kangseong@gmail.com>
2016-09-29 02:00:44 +0900

    Remove unnecessary file and codes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,5 +41,5 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.browserify_rails.node_env = "development"
+  # config.browserify_rails.node_env = "development"
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.browserify_rails.node_env = "production"
+  # config.browserify_rails.node_env = "production"
 end
