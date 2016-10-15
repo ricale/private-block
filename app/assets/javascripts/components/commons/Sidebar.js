6514a8506a975ace6db95e2ddcd092c37942f989
KIM Kangseong <KIM.Kangseong@gmail.com>
2016-10-15 15:57:50 +0900

    Add toggle button to sidebar for show/hide sidebar contents

--- app/assets/javascripts/components/commons/Sidebar.js
+++ app/assets/javascripts/components/commons/Sidebar.js
@@ -3,14 +3,25 @@ import React, { Component } from 'react'
 import CategoryList from '../categories/CategoryList'
 
 export default class Sidebar extends Component {
-  
+  state = {
+    show: false
+  }
+
+  onToggleButtonClick () {
+    this.setState({show: !this.state.show})
+  }
 
   render () {
     const { className, categories } = this.props
 
+    const visibleClassName = this.state.show ? '' : 'hide'
+
     return (
-      <div className={className || ''}>
-        <CategoryList categories={categories.list} hideMenu={true} />
+      <div className={`sidebar ${className || ''}`}>
+        <span className='sidebar__toggle-button' onClick={this.onToggleButtonClick.bind(this)}>button</span>
+        <div className={`sidebar__contents ${visibleClassName}`}>
+          <CategoryList categories={categories.list} hideMenu={true} />
+        </div>
       </div>
     )
   }
