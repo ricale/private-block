1000506b3e69b11c5ee6ee0f095f8509ee753013
ricale <KIM.kangseong@gmail.com>
2017-08-18 00:58:37 +0900

    Update post order and Clear css codes

--- weblog/views.py
+++ weblog/views.py
@@ -5,7 +5,7 @@ from .models import Post, Comment
 from .forms import PostForm, CommentForm
 
 def post_list(request):
-  posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+  posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
   return render(request, 'weblog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -41,7 +41,7 @@ def post_edit(request, pk):
 
 @login_required
 def post_draft_list(request):
-  posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+  posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
   return render(request, 'weblog/post_draft_list.html', {'posts': posts})
 
 @login_required
