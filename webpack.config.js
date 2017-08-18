0c13d0fdd94f7370222cf2d3c6d4a54edba952e9
ricale <KIM.kangseong@gmail.com>
2017-08-19 01:05:42 +0900

    Add babel and Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,24 @@ module.exports = {
     filename: "[name]-[hash].js"
   },
 
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|es6)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['react', 'es2015', 'stage-2']
+            }
+          }
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+
   plugins: [
     new BundleTracker({filename: './webpack-stats.json'})
-  ]
+  ],
 };
